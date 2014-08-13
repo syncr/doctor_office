6fa654cb39bc42c7557af331e469c564b9dc5533
Jeremy Whitaker & Ruben Rhodes <student@epicodus.com>
2014-08-13 14:16:14 -0700

    provide initial file structure and requires

---
+++ README.md
@@ -0,0 +1,13 @@
+Doctors Office Database
+=======================
+A database created for the Epicodus class.
+Contains the following classes
+> Doctors
+> Patients
+> Appoinments
+> Insurance
+
+Authors:
+Jeremy Whitaker
+Ruben Rhodes
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require 'doctor'
+require 'patient'
+require 'appointment'
+require 'insurance'
+require 'pg'
+
+DB = PG.connect({:dbname => 'medical_system_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM doctor *;")
+    DB.exec("DELETE FROM patient *;")
+    DB.exec("DELETE FROM appointment *;")
+    DB.exec("DELETE FROM insurance *;")
+  end
+end
+
